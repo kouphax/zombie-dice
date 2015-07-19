630b5b1e2334a9592df78117d3a14469988cace1
James Hughes <james@yobriefca.se>
2015-07-19 20:50:26 +0100

    Automated Commit

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+set -e
+
+rm -rf target/
+git clone git@github.com:kouphax/zombie-dice.git --branch gh-pages target
+boot prepare
+
+cd target
+git add -A
+git commit -m "Automated Commit"
+git push origin gh-pages
+cd ..
+rm -rf target
